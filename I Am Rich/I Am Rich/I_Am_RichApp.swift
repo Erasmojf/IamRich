3accfa01bac54a609aea93ca79dbd162f5af2563
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-05-18 16:12:40 -0300

    Criando App I Am Rich
    Criando App I Am Rich

---
+++ I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Erasmo J.F Da Silva on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
